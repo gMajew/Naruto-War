1693333623f623aef128f6c7663158c14e572bfe
gMajew <gael.majewski@gmail.com>
2024-04-12 09:27:05 +0200

    reorga

---
+++ public/Game/phaser/Scene1.js
@@ -0,0 +1,22 @@
+class Scene1 extends Phaser.Scene {
+  constructor() {
+    super("bootGame");
+  }
+
+  preload() {
+    this.load.image("background", "./img/Background_naruto.png");
+    this.load.spritesheet("naruto", "./img/narutess.png", {
+      frameWidth: 60,
+      frameHeight: 60,
+    });
+    this.load.spritesheet("mob", "./img/mobs.png", {
+      frameWidth: 60,
+      frameHeight: 60,
+    });
+  }
+
+  create() {
+    this.add.text(20, 20, "Loading game...");
+    this.scene.start("playGame");
+  }
+}
